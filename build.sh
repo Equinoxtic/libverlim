e5ac359e83471faa3f9b08544ea0052867fa13e7
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-01-25 13:42:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,14 @@ then
 	mkdir bin
 fi
 
+if [ ! -d logs ]
+then
+	mkdir logs
+	if [ ! -f log.txt  ]
+	then
+		touch logs/log.txt
+	fi
+fi
+
 cmake -G "Visual Studio 17 2022" -A Win32 -S . -B bin
 cmake --build bin --target amixdemo --config Release
