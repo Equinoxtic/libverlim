8cd2699f68997367390285f0037f3e560b586e95
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-01-27 13:57:39 +0800

    Time formatting implementation

--- lib/timelib.h
+++ lib/timelib.h
@@ -3,6 +3,7 @@
 
 #include<string>
 #include "vtime.h"
+#include "stringutils.h"
 
 static inline std::string get_time() {
 	std::string tm = DEF_TIME; return tm;
@@ -12,8 +13,25 @@ static inline std::string get_date() {
 	std::string dt = DEF_DATE; return dt;
 }
 
-std::string get_time_and_date() {
+static inline std::string get_time_and_date() {
 	std::string tmdt = DEF_TIME_AND_DATE; return tmdt;
+}
+
+static inline std::string fmt_time(int tm = 1, int dt = 0) {
+	std::string tm_s,
+				dt_s;
+	tm_s = (tm==1) ? DEF_TIME : "";
+	dt_s = (dt==1) ? DEF_DATE : "";
+	
+	std::string fmt_s;
+	if (!str_empty(tm_s)) {
+		if (str_empty(dt_s))
+			fmt_s = tm_s;
+		else
+			fmt_s = tm_s +  " : " + dt_s;
+	}
+
+	return fmt_s;
 }
 
 #endif // TIMELIB_H_
