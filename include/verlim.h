b8956d72df0a7c63dca3db3750e84d7f68fd2324
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-02-07 00:49:39 +0800

    Small bug fix and updates

---
+++ include/verlim.h
@@ -0,0 +1,12 @@
+#include "stringutils.h"
+#include "thread.h"
+#include "fs/fileutils.h"
+#include "fs/file.h"
+#include "sys/limsys.h"
+#include "sys/limbuiltin.h"
+#include "limio/limstdio.h"
+#include "limio/limioutils.h"
+#include "logger.h"
+#include "version.h"
+#include<iostream>
+#include<string>
