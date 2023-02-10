2994aad9561849a7c06c4ffa722b232076086084
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-02-10 09:20:12 +0800

    Update `stringutils`

--- lib/stringutils.cpp
+++ lib/stringutils.cpp
@@ -18,6 +18,18 @@ bool starts_with(const std::string& str, const std::string& prefix)
 	return (str.size() >= prefix.size() && str.compare(0, prefix.size(), prefix)) == 0;
 }
 
+bool find_str(std::string s, std::string targ_s)
+{
+	bool found = false;
+	std::size_t s_npos = std::string::npos,
+				found_s = s.find(targ_s);
+	if (!s.empty() && !targ_s.empty())
+		found = (found_s != s_npos);
+	else
+		found = false;
+	return found;
+}
+
 bool compare_str(std::string a, std::string b)
 {
 	return (a == b);
--- lib/stringutils.h
+++ lib/stringutils.h
@@ -6,6 +6,7 @@
 std::string wrap_str(std::string s, std::string a, std::string b);
 std::string quote_str(std::string s);
 bool starts_with(const std::string& str, const std::string& prefix);
+bool find_str(std::string s, std::string targ_s);
 bool compare_str(std::string a, std::string b);
 bool str_empty(std::string s);
 
