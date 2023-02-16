0178ce1e1617a91b5754ae8db6aeedd0f9070d6b
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-02-16 20:34:22 +0800

    Update scripts

--- build.ps1
+++ build.ps1
@@ -2,12 +2,5 @@ if (-not([System.IO.File]::Exists("bin/"))) {
 	New-Item -ItemType "directory" "bin/"
 }
 
-if (-not([System.IO.File]::Exists("logs/"))) {
-	New-Item -ItemType "directory" "logs/"
-	if (-not([System.IO.File]::Exists("logs/log.txt"))) {
-		New-Item -ItemType "file" "logs/log.txt"
-	}
-}
-
 cmake -G "Visual Studio 17 2022" -A Win32 -S . -B bin
 cmake --build bin --target verlimdemo --config Release
