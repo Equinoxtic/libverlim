51e083b170c0c2b7e47d2f0d860539b3e00f8328
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-02-25 23:58:32 +0800

    Create `display` addons

---
+++ addons/display/bar.h
@@ -0,0 +1,9 @@
+#ifndef BAR_H_
+#define BAR_H_
+
+#include<string>
+#include<vector>
+
+void create_bar(unsigned delay = 25, int len = 15);
+
+#endif // BAR_H_
---
+++ addons/display/seperator.h
@@ -0,0 +1,9 @@
+#ifndef SEPERATOR_H_
+#define SEPERATOR_H_
+
+#include<string>
+
+void create_seperator(int length = 65);
+void formatted_seperator(std::string s, int length = 65);
+
+#endif // SEPERATOR_H_
