0d592e04736b65f7587b00b94d01ebf8030bc329
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-02-26 09:57:34 +0800

    Create `mem` library

---
+++ lib/mem.h
@@ -0,0 +1,87 @@
+#ifndef _WIN32
+
+#ifndef MEM_H_
+#define MEM_H_
+
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+#include "sys/times.h"
+#include "sys/vtimes.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+static inline long long get_total_virtmem() {
+	struct sysinfo meminfo;
+	sysinfo (&meminfo);
+	long long totalvirtmem = meminfo.totalram;
+	totalvirtmem += meminfo.totalswap;
+	totalvirtmem *= meminfo.mem_unit;
+	return totalvirtmem;
+}
+
+static inline long long get_used_virtmem() {
+	struct sysinfo meminfo;
+	sysinfo (&meminfo);
+	long long usedvirtmem = meminfo.totalram;
+	usedvirtmem += meminfo.totalswap - meminfo.freeswap;
+	usedvirtmem *= meminfo.mem_unit;
+	return usedvirtmem;
+}
+
+int parse_line(char *line) {
+	int i = strlen(line);
+	const char *p = line;
+	while (*p < '0' || *p > '9') p++;
+	line[i - 3] = '\0';
+	i = atoi(p);
+	return i;
+}
+
+static inline int get_cur_virtmem() {
+	FILE *f = fopen("/proc/self/status", "r");
+	int res = -1;
+	char line[128];
+	while (fgets(line, 128, f) != NULL) {
+		if (strncmp(line, "VmSize:", 7) == 0) {
+			res = parse_line(line);
+			break;
+		}
+	}
+	fclose(file);
+	return res;
+}
+
+static inline long long get_total_physmem() {
+	struct sysinfo meminfo;
+	sysinfo (&meminfo);
+	long long totalphysmem = meminfo.totalram;
+	totalphysmem *= meminfo.mem_unit;
+	return totalphysmem;
+}
+
+static inline long long get_used_physmem() {
+	struct sysinfo meminfo;
+	sysinfo (&meminfo);
+	long long usedvirtmem = meminfo.totalram;
+	usedvirtmem += meminfo.totalram - meminfo.freeram;
+	return usedvirtmem;
+}
+
+static inline int get_cur_physmem() {
+	FILE *f = fopen("/proc/self/status", "r");
+	int res = -1;
+	char line[128];
+	while (fgets(line, 128, f) != NULL) {
+		if (strncmp(line, "VmRSS:", 6) == 0) {
+			res = parse_line(line);
+			break;
+		}
+	}
+	fclose(file);
+	return res;
+}
+
+#endif // MEM_H_
+
+#endif
