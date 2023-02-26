e641a2704a4c92165e4adf29160c23da40ab96eb
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-02-26 09:58:08 +0800

    Just doin' some bug fixes

--- addons/display/bar.cpp
+++ addons/display/bar.cpp
@@ -37,19 +37,3 @@ void create_bar(unsigned delay, int len)
 			std::cout << repeat_str("\b", nbar.size());
 	}
 }
-
-void formatted_bar(std::vector<std::string> bar_vec, std::string bar_s, unsigned delay, int len, int increase_inc, size_t clear_inc)
-{
-	unsigned ndelay = u_bar_check(delay, 25, 150);
-	int nlen = bar_check(len, 1, 3);
-	
-	for (int i = 0; i < nlen; ++i) {
-		for (int j = 0; j < (nlen - nlen) + 1; ++j)
-			bar_vec.insert(bar_vec.begin() + increase_inc, bar_s);
-		for (int k = 0; k < bar_vec.size(); ++k)
-			std::cout << bar_vec[k];
-		ms_sleep(750);
-		for (int x = 0; x < (nlen - nlen) + 1; ++x)
-			std::cout << repeat_str("\b", clear_inc);
-	}
-}
