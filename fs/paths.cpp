9de6d534cc2c5e3cffbb101671036d4ad775602b
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-03-07 20:52:45 +0800

    Update `fs` library

--- fs/paths.cpp
+++ fs/paths.cpp
@@ -1,15 +1,17 @@
 #include "./paths.h"
 #include "../lib/stringutils.h"
 
-std::string get_file(std::string f, std::string ext)
-{
-	std::string rf;
-	if (!str_empty(f) && !str_empty(ext))
-		rf = f+"."+ext;
-	return rf;
-}
+namespace lvfs {
+	std::string get_file(std::string f, std::string ext)
+	{
+		std::string rf;
+		if (!str_empty(f) && !str_empty(ext))
+			rf = f+"."+ext;
+		return rf;
+	}
 
-std::string get_log_file()
-{
-	return get_file("logs/log", "txt");
+	std::string get_log_file()
+	{
+		return get_file("logs/log", "txt");
+	}
 }
