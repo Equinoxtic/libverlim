a09e336086652410d58a42cd5a53b4c69f0530ca
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-03-07 20:52:56 +0800

    Update `logging` library

--- logging/consolemsg.h
+++ logging/consolemsg.h
@@ -3,13 +3,15 @@
 
 #include<string>
 
-std::string create_console_msg(std::string c, bool include_time = true);
-void out_console_msg(std::string c, bool includetime = true);
-std::string create_consolemsg_flag(std::string flagtype = "err",
-								   std::string target = "TARGET", 
-								   std::string reason = "REASON");
-void out_consolemsg_flag(std::string flagtype = "err", 
-						 std::string target = "TARGET", 
-						 std::string reason = "REASON");
+namespace lconsole {
+	std::string create_console_msg(std::string c, bool include_time = true);
+	void out_console_msg(std::string c, bool includetime = true);
+	std::string create_consolemsg_flag(std::string flagtype = "err",
+									   std::string target = "TARGET", 
+									   std::string reason = "REASON");
+	void out_consolemsg_flag(std::string flagtype = "err", 
+							 std::string target = "TARGET", 
+							 std::string reason = "REASON");
+}
 
 #endif // CONSOLEMSG_H_
--- logging/logger.cpp
+++ logging/logger.cpp
@@ -1,39 +1,47 @@
 #include "./logger.h"
 #include "./consolemsg.h"
 #include "../lib/stringutils.h"
+#include "../lib/vectorutils.h"
 #include "../fs/file.h"
 #include "../fs/fileutils.h"
+#include<vector>
 
-void log_message(std::string msg, std::string logfile_path)
-{
-	std::string f_msg;
-	if (!str_empty(msg))
-		f_msg = create_console_msg(msg, true);
-	if (file_exists(logfile_path))
-		write_to_file(logfile_path, f_msg);
-}
-
-void advanced_log(std::string msg,
-				  std::string msg_code,
-				  std::string addinfo,
-				  std::string logfile_path)
-{
-	std::string f_msg;
-	if (!str_empty(msg)) {
-		if (str_empty(msg_code))
-			f_msg = create_console_msg(msg, true);
-		else
-			f_msg = create_console_msg(msg, true) + wrap_str(msg_code, "(", ")");
+namespace logger {
+	void log_message(std::string msg, std::string logfile_path)
+	{
+		std::string f_msg;
+		if (!str_empty(msg))
+			f_msg = lconsole::create_console_msg(msg, true);
+		if (lvfs::file_exists(logfile_path))
+			lvfs::write_to_file(logfile_path, f_msg);
 	}
 
-	std::string f_info;
-	if (!str_empty(addinfo))
-		f_info = "\nAdditional Info: " + quote_str(addinfo);
+	void advanced_log(std::string msg,
+					std::string msg_code,
+					std::string addinfo,
+					std::string logfile_path)
+	{
+		std::string f_msg;
+		std::vector<std::string> vec_s = {
+			lconsole::create_console_msg(msg, true), " "
+		};
 
-	write_to_file(logfile_path, f_msg + f_info);
-}
+		if (!str_empty(msg)) {
+			if (str_empty(msg_code))
+				f_msg = ret_vector_s(vec_s);
+			else
+				vec_s.push_back(wrap_str(msg_code, "(", ")"));
 
-void clear_logfile(std::string path)
-{
-	clear_file(path);
+			if (!str_empty(addinfo))
+				vec_s.push_back("\nAdditional Info: " + quote_str(addinfo));
+			f_msg = ret_vector_s(vec_s);
+		}
+
+		lvfs::write_to_file(logfile_path, f_msg);
+	}
+
+	void clear_logfile(std::string path)
+	{
+		lvfs::clear_file(path);
+	}
 }
