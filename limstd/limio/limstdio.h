d20a7a679192202947d8ea074010a01e3f8568e9
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-03-07 20:53:11 +0800

    Update `limstd`

--- limstd/limio/limstdio.h
+++ limstd/limio/limstdio.h
@@ -6,35 +6,21 @@
 #include<string>
 #include<conio.h>
 
-inline static void printstr(std::string c, std::string end = "\n") {
-	std::cout << c << (compare_str(end, "\n") ? "\n" : end);
-}
+namespace lstd
+{
+	template<typename T>
+	inline static void prints(T c, std::string end = "\n") {
+		std::cout << c << (compare_str(end, "\n") ? "\n" : end);
+	}
 
-inline static void printint(int n, std::string end = "\n") {
-	std::cout << n << (compare_str(end, "\n") ? "\n" : end);
-}
+	template<typename T>
+	inline static void reads(T &s) {
+		std::getline(std::cin, s);
+	}
 
-inline static void printfl(float n, std::string end = "\n") {
-	std::cout << n << (compare_str(end, "\n") ? "\n" : end);
-}
-
-inline static void printbool(bool x, std::string end = "\n") {
-	std::string bool_s = ((x==true) ? "true" : "false");
-	std::cout << bool_s << (compare_str(end, "\n") ? "\n" : end);
-}
-
-inline static void printnbool(int x, std::string end = "\n") {
-	std::string bool_s = ((x>=1) ? "true" : "false");
-	std::cout << bool_s << (compare_str(end, "\n") ? "\n" : end);
-}
-
-inline static void readstr(std::string &s) {
-	std::getline(std::cin, s);
-}
-
-inline static void readch(char* ch, int size) {
-	for (int i = 0; i < size; ++i) {
-		ch[i] = getch();
+	template<typename T>
+	inline static void readch(T &ch, int size) {
+		ch = getch(); 
 	}
 }
 
