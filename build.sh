f322edaf04231c8d1d9423d59a41144adbb38ed4
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-03-08 09:24:41 +0800

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-if [ ! -d build  ]
-then
+if [ ! -d build  ]; then
 	mkdir build
+fi
+
+if [ ! -d logs ]; then
+	mkdir logs
+fi
+
+if [ ! -f logs/log.txt ]; then
+	touch logs/logfile.txt
 fi
 
 cmake -G "Visual Studio 17 2022" -A Win32 -S . -B build
