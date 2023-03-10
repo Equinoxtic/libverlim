f83c2c6819666fb82b1bf240b8b33b28236a263f
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-03-10 19:08:41 +0800

    Update `help` command

--- bin/help/help.cpp
+++ bin/help/help.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<string>
+#include<cstring>
+#include<cstdlib>
 #include<vector>
 #include "../../addons/libcommand/commandutils.h"
 
-void list_commands()
+void list_commands(bool verbose = true)
 {
 	std::vector<std::string> list = {
 		"help",
@@ -20,8 +22,23 @@ void list_commands()
 		"Install LibVerlim resource files. (For customization and etc.)"
 	};
 	std::cout << "\n";
-	c_command_s(list, descriptions);
-	std::cout << "\n";
+	if (verbose) {
+		c_command_s(list, descriptions);
+		std::cout << "\n";
+	} else {
+		for (size_t i = 0; i < list.size(); ++i)
+			std::cout << list[i] << " ";
+		std::cout << "\n\n";
+	}
 }
 
-int main(int argc, char *argv[]) { list_commands(); return 0; }
+int main(int argc, char *argv[]) {
+	for (int i = 0; i < argc; ++i) {
+		if (strcmp(argv[i], "-v") == 0) {
+			list_commands();
+		} else if (strcmp(argv[i], "-s") == 0) {
+			list_commands(false);
+		}
+	}
+	return 0;
+}
