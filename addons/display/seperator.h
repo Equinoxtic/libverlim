073e754564aec2146bfd663bd80467550e1397b4
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-03-20 19:05:40 +0800

    Update `seperator`

--- addons/display/seperator.h
+++ addons/display/seperator.h
@@ -8,9 +8,22 @@ typedef struct Seperator {
 } Seperator;
 
 std::string create_seperator(int length = 65);
-std::string formatted_seperator(std::string s, int length = 65);
+
+std::string formatted_seperator(
+	std::string s,
+	int length = 65,
+	std::string begin = "",
+	std::string end = ""
+);
+
 void set_seperator(Seperator *sepptr, int length = 65);
-void set_formatted_seperator(Seperator *sepptr, std::string s, int length = 65);
+
+void set_formatted_seperator(
+	Seperator *sepptr, std::string s,
+	int length = 65, 
+	std::string end = ""
+);
+
 void display_seperator(Seperator *seperator);
 
 #endif // SEPERATOR_H_
