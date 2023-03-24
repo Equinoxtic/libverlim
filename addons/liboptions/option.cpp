425c3645e1316f8bd47b2f4aa7bcf85479d7498b
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-03-25 00:33:18 +0800

    Update `liboptions`
    New options listing functions

--- addons/liboptions/option.cpp
+++ addons/liboptions/option.cpp
@@ -39,6 +39,31 @@ void set_option(Option *option, std::string opt_n, std::string opt_s) {
 	option->data = create_namedoption(opt_n, opt_s);
 }
 
+std::string create_options_list(std::vector<std::pair<int, std::string>> vec_o) {
+	std::stringstream vf, sn;
+	for (size_t i = 0; i < vec_o.size(); ++i) {
+		sn << vec_o[i].first;
+		vf << wrap_str(sn.str(), "[", "]") << ": " << vec_o[i].second << "/";
+	}
+	return wrap_str(vf.str(), "(", ")");
+}
+
+std::string create_namedoptions_list(std::vector<std::pair<std::string, std::string>> vec_o) {
+	std::stringstream vf;
+	for (size_t i = 0; i < vec_o.size(); ++i) {
+		vf << wrap_str(vec_o[i].first, "[", "]") << ": " << vec_o[i].second << "/";
+	}
+	return wrap_str(vf.str(), "(", ")");
+}
+
+void set_options_list(Option *option, std::vector<std::pair<int, std::string>> vec_o) {
+	option->data = create_options_list(vec_o);
+}
+
+void set_namedoptions_list(Option *option, std::vector<std::pair<std::string, std::string>> vec_o) {
+	option->data = create_namedoptions_list(vec_o);
+}
+
 void put_option(Option *option) {
 	std::cout << option->data;
 }
--- addons/liboptions/option.h
+++ addons/liboptions/option.h
@@ -3,6 +3,7 @@
 
 #include<string>
 #include<iostream>
+#include<vector>
 
 typedef struct Option {
 	std::string data;
@@ -12,6 +13,10 @@ std::string create_option(int opt_n, std::string opt_s);
 std::string create_namedoption(std::string opt_n, std::string opt_s);
 void set_option(Option *option, int opt_n, std::string opt_s);
 void set_option(Option *option, std::string opt_n, std::string opt_s);
+std::string create_options_list(std::vector<std::pair<int, std::string>> vec_o);
+std::string create_namedoptions_list(std::vector<std::pair<std::string, std::string>> vec_o);
+void set_options_list(Option *option, std::vector<std::pair<int, std::string>> vec_o);
+void set_namedoptions_list(Option *option, std::vector<std::pair<std::string, std::string>> vec_o);
 void put_option(Option *option);
 
 #endif // OPTION_H_
